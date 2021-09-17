c49ff1eac101947a6e03de8c8c198b572daed6ef
Ortrvis <90830156+Ortrvis@users.noreply.github.com>
2021-09-17 10:50:53 +0200

    výpis žáku
    výpis žáku

---
+++ pole.py
@@ -0,0 +1,4 @@
+x = ["IT1A-Pocet zaku-16", "IT1B-Pocet zaku-17", "IT2A-Pocet zaku-19", "IT2B-Pocet zaku-14","IT3-Pocet zaku-20", "IT4-Pocet zaku-16"]
+slovnik = {"IT1B" : 17, "IT1A" : 17, "It3" : 13}
+for trida in x:
+    print(trida)
---
+++ zaci.py
@@ -0,0 +1,62 @@
+x=".............."
+print(f"{x}")
+tridy = [
+    {"Oznaceni": "IT1A", "Pocet zaku": 16},  
+    
+]
+print(f'Oznaceni: {tridy[0]["Oznaceni"]}')
+print(f'Pocet zaku: {tridy[0]["Pocet zaku"]}')
+print(f"{x}")
+
+
+tridy = [
+    {"Oznaceni": "IT1B", "Pocet zaku": 17},
+]
+print(f'Oznaceni: {tridy[0]["Oznaceni"]}')
+print(f'Pocet zaku: {tridy[0]["Pocet zaku"]}')
+print(f"{x}")
+
+
+tridy = [
+    {"Oznaceni": "IT2A", "Pocet zaku": 20}, 
+]
+print(f'Oznaceni: {tridy[0]["Oznaceni"]}')
+print(f'Pocet zaku: {tridy[0]["Pocet zaku"]}') 
+print(f"{x}")
+
+
+tridy = [
+    {"Oznaceni": "IT2B", "Pocet zaku": 19},
+]
+print(f'Oznaceni: {tridy[0]["Oznaceni"]}')
+print(f'Pocet zaku: {tridy[0]["Pocet zaku"]}')
+print(f"{x}")
+
+
+tridy = [
+    {"Oznaceni": "IT3A", "Pocet zaku": 15}, 
+]
+print(f'Oznaceni: {tridy[0]["Oznaceni"]}')
+print(f'Pocet zaku: {tridy[0]["Pocet zaku"]}')
+print(f"{x}")
+
+
+tridy = [
+    {"Oznaceni": "IT3B", "Pocet zaku": 14}, 
+]
+print(f'Oznaceni: {tridy[0]["Oznaceni"]}')
+print(f'Pocet zaku: {tridy[0]["Pocet zaku"]}')
+print(f"{x}")
+
+tridy = [
+    {"Oznaceni": "IT1A", "Pocet zaku": 16},  
+    {"Oznaceni": "IT1B", "Pocet zaku": 17},
+    {"Oznaceni": "IT2A", "Pocet zaku": 20}, 
+    {"Oznaceni": "IT2B", "Pocet zaku": 19},
+    {"Oznaceni": "IT3A", "Pocet zaku": 15},
+    {"Oznaceni": "IT3B", "Pocet zaku": 14}, 
+    
+]
+print(f'Oznaceni: {tridy[0]["Oznaceni"]}')
+print(f'Pocet zaku: {tridy[0]["Pocet zaku"]}')
+
